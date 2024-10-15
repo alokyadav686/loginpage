2af7a5d2a7f06812bdd7f3d29e50d13ff3239c8c
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-10-16 01:05:04 +0530

    pass equal

--- signup.js
+++ signup.js
@@ -3,6 +3,17 @@ let opens = document.querySelectorAll("#close")
 let closeds = document.querySelectorAll("#open")
 
 
+let a =document.querySelector("#a")
+let b =document.querySelector("#b")
+let c =document.querySelector("#c")
+let d =document.querySelector("#d")
+let e =document.querySelector("#e")
+
+let checkbox = document.querySelector("#cbox")
+
+let register =document.querySelector(".registerbtn")
+
+
 
 
 passwords.forEach((password,id) =>{
@@ -35,4 +46,53 @@ passwords.forEach((password,id) =>{
         
     })
     
+})
+
+
+const data=()=>{
+
+if(a.value=="" || b.value=="" || c.value=="" || d.value==""|| e.value=="" ){
+    alert("all fields are mandatory")
+    return false;
+}
+else{
+    return true
+}
+}
+
+
+const equalpass =()=>{
+    
+    if(b.value!= c.value){
+        alert("Password dont match!!")
+        b.style.border ="2px solid red"
+        c.style.border ="2px solid red"
+        
+    }
+    else{
+        b.style.border =""
+        c.style.border =""
+        return true
+    }
+    
+}
+const checkbtn =()=>{
+    if(checkbox.checked){
+        return true
+    }
+    else{
+        alert("checkbox is mandatory")
+        return false
+        
+    }
+}
+register.addEventListener("click", ()=>{
+
+    data()
+    equalpass()
+    checkbtn()
+
+        // if(data() && equalpass()&&checkbox()){
+        //     console.log("everything is good")
+        // }
 })
