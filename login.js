08d087f43ab9c8b5956e039fad08faf999b64d1c
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-10-15 22:10:57 +0530

    password show /hide done

---
+++ login.js
@@ -0,0 +1,28 @@
+let password = document.querySelector(".passcon input")
+let open = document.querySelector("#close")
+let closed = document.querySelector("#open")
+
+
+
+closed.style.display = "none"
+
+
+open.addEventListener("click", () => {
+    if (password.type === "password") {
+        password.type = "text"
+        open.style.display = "none"
+        closed.style.display = "inline"
+
+    }
+
+})
+
+closed.addEventListener("click", () => {
+    if (password.type === "text") {
+        password.type = "password"
+        closed.style.display = "none"
+        open.style.display = "inline"
+ 
+    }
+
+})
