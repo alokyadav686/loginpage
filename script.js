cf538eecb48e24c38f6da5c994661ddf289f64d2
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-10-16 20:13:24 +0530

    rating added

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let cimg = document.querySelectorAll(".andar")
 let next = document.querySelector(".btnright")
 let previous = document.querySelector(".btnleft")
 
-let allstars =document.querySelectorAll(".stars i")
+let allstars1 =document.querySelectorAll(".stars1 i")
+let allstars2 =document.querySelectorAll(".stars2 i")
+let allstars3 =document.querySelectorAll(".stars3 i")
+let allstars4 =document.querySelectorAll(".stars4 i")
 
 
 let counter=0
@@ -52,7 +55,8 @@ setInterval(() => {
 }, 4000);
 
 
-allstars.forEach((faStar, i) =>{
+
+allstars1.forEach((faStar, i) =>{
 
     // console.log(faStar)
     // console.log(allstars[i])
@@ -62,7 +66,74 @@ allstars.forEach((faStar, i) =>{
         console.log(i)
         const n=i
         for (i=0;i<=n;i++){
-            
+            if(i<=n){
+
+                allstars1[i].style.color="yellow"
+            }
+
+        }
+
+    })
+
+})
+
+allstars2.forEach((faStar, i) =>{
+
+    // console.log(faStar)
+    // console.log(allstars[i])
+    // console.log(i)
+
+    faStar.addEventListener("click", ()=>{
+        console.log(i)
+        const n=i
+        for (i=0;i<=n;i++){
+            if(i<=n){
+
+                allstars2[i].style.color="yellow"
+            }
+
+        }
+
+    })
+
+})
+
+allstars3.forEach((faStar, i) =>{
+
+    // console.log(faStar)
+    // console.log(allstars[i])
+    // console.log(i)
+
+    faStar.addEventListener("click", ()=>{
+        console.log(i)
+        const n=i
+        for (i=0;i<=n;i++){
+            if(i<=n){
+
+                allstars3[i].style.color="yellow"
+            }
+
+        }
+
+    })
+
+})
+
+allstars4.forEach((faStar, i) =>{
+
+    // console.log(faStar)
+    // console.log(allstars[i])
+    // console.log(i)
+
+    faStar.addEventListener("click", ()=>{
+        console.log(i)
+        const n=i
+        for (i=0;i<=n;i++){
+            if(i<=n){
+
+                allstars4[i].style.color="yellow"
+            }
+
         }
 
     })
